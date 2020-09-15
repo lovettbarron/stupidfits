2d6082b29ff95e01b27f7e10e1595ac2a7d03b0f
Andrew Lovett-Barron <alb@andrewlb.com>
2020-09-15 11:34:19 +0200

    Testing

--- pages/api/auth/insta.js
+++ pages/api/auth/insta.js
@@ -29,28 +29,51 @@ class InstagramBasicDisplayApi {
   }
 
   retrieveToken(userCode) {
-    const form = new FormData();
-    form.append("client_id", this._appId);
-    form.append("client_secret", this._appSecret);
-    form.append("grant_type", "authorization_code");
-    form.append("redirect_uri", this._redirectUri);
-    form.append("code", userCode.replace("#_", ""));
-
-    console.log(
-      "Token Request",
-      `${INSTAGRAM_GRAPH_BASE_URL}/access_token`,
-      form
+    var data = new FormData();
+    data.append("client_id", "325074402038126");
+    data.append("client_secret", "75fc7af89421912dc3c4f11d182ffb32");
+    data.append("grant_type", "authorization_code");
+    data.append("redirect_uri", "https://stage.stupidfits.com/api/auth/insta");
+    data.append(
+      "code",
+      "AQCuazNU_Fw54RsbY5j6VB2aeMAr6kwdii4v8PRSBxoxMwTuPgQhUR85szpBhNVtjVFDyUO0Q98P0ZM1_b_pg_566TeNvyz2SYnd-Soz9VAobKDTcobkKji-QYnrWoEzAZCOF49dI3LRzM6-rWpJmJ4slojoiCSdGLPPH1FqH0aXYIH5KMvjGpomf6wGdM_DhB7ufebSNhooCz68kJDk2Kh7yuaNRxZTZLA-OU6XUIL7SQ"
     );
 
-    const config = {
+    var config = {
       method: "post",
-      url: `${INSTAGRAM_GRAPH_BASE_URL}/access_token`,
+      url: "https://api.instagram.com/oauth/access_token",
       headers: {
-        ...form.getHeaders(),
+        ...data.getHeaders(),
       },
-      data: form,
+      data: data,
     };
-    return axios(config).then((res) => res.data);
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    // const form = new FormData();
+    // form.append("client_id", this._appId);
+    // form.append("client_secret", this._appSecret);
+    // form.append('grant_type', 'authorization_code');
+    // form.append("redirect_uri", this._redirectUri);
+    // form.append("code", userCode.replace("#_", ""));
+
+    // console.log("Token Request", `${INSTAGRAM_GRAPH_BASE_URL}/access_token`);
+
+    // const config = {
+    //   method: "post",
+    //   url: `${INSTAGRAM_GRAPH_BASE_URL}/access_token`,
+    //   headers: {
+    //     ...form.getHeaders(),
+    //   },
+    //   data: form,
+    // };
+    // return axios(config).then((res) => res.data);
     // .post(`${INSTAGRAM_GRAPH_BASE_URL}/access_token`, form, {
     //   headers: {
     //     // "Content-Type": "multipart/form-data",
