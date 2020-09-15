2d9b02a319e0f691c420f58f4b737aae388fa5a9
Andrew Lovett-Barron <alb@andrewlb.com>
2020-09-15 12:00:41 +0200

    This should work?

--- pages/api/auth/insta.js
+++ pages/api/auth/insta.js
@@ -1,8 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import { getSession, session } from "next-auth/client";
-const axios = require("axios");
-const querystring = require("querystring");
-const FormData = require("form-data");
+import axios from "axios";
+import querystring from "querystring";
+import FormData from "form-data";
+import request from "request";
+import qs from "qs";
 const prisma = new PrismaClient();
 
 const INSTAGRAM_OAUTH_BASE_URL = "https://api.instagram.com/oauth";
@@ -29,57 +31,27 @@ class InstagramBasicDisplayApi {
   }
 
   retrieveToken(userCode) {
-    var data = new FormData();
-    data.append("client_id", "325074402038126");
-    data.append("client_secret", "75fc7af89421912dc3c4f11d182ffb32");
-    data.append("grant_type", "authorization_code");
-    data.append("redirect_uri", "https://stage.stupidfits.com/api/auth/insta");
-    data.append(
-      "code",
-      "AQCuazNU_Fw54RsbY5j6VB2aeMAr6kwdii4v8PRSBxoxMwTuPgQhUR85szpBhNVtjVFDyUO0Q98P0ZM1_b_pg_566TeNvyz2SYnd-Soz9VAobKDTcobkKji-QYnrWoEzAZCOF49dI3LRzM6-rWpJmJ4slojoiCSdGLPPH1FqH0aXYIH5KMvjGpomf6wGdM_DhB7ufebSNhooCz68kJDk2Kh7yuaNRxZTZLA-OU6XUIL7SQ"
-    );
-
+    var data = qs.stringify({
+      client_id: this._appId,
+      client_secret: this._appSecret,
+      grant_type: "authorization_code",
+      redirect_uri: this._redirectUri,
+      code: userCode.replace("#_", ""),
+    });
     var config = {
       method: "post",
-      url: "https://api.instagram.com/oauth/access_token",
+      url: `${INSTAGRAM_GRAPH_BASE_URL}/access_token`,
       headers: {
-        ...data.getHeaders(),
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       data: data,
     };
 
-    axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      })
+    return axios(config)
+      .then((res) => res.data)
       .catch(function (error) {
         console.log(error);
       });
-
-    // const form = new FormData();
-    // form.append("client_id", this._appId);
-    // form.append("client_secret", this._appSecret);
-    // form.append('grant_type', 'authorization_code');
-    // form.append("redirect_uri", this._redirectUri);
-    // form.append("code", userCode.replace("#_", ""));
-
-    // console.log("Token Request", `${INSTAGRAM_GRAPH_BASE_URL}/access_token`);
-
-    // const config = {
-    //   method: "post",
-    //   url: `${INSTAGRAM_GRAPH_BASE_URL}/access_token`,
-    //   headers: {
-    //     ...form.getHeaders(),
-    //   },
-    //   data: form,
-    // };
-    // return axios(config).then((res) => res.data);
-    // .post(`${INSTAGRAM_GRAPH_BASE_URL}/access_token`, form, {
-    //   headers: {
-    //     // "Content-Type": "multipart/form-data",
-    //     "content-type": "application/x-www-form-urlencoded",
-    //   },
-    // })
   }
 
   retrieveLongLivedToken(accessToken) {
@@ -177,6 +149,8 @@ export default async function handle(req, res) {
   if (req.query.code) {
     const code = req.query.code; // Auth code
     const token = await ig.retrieveToken(code);
+    console.log(token);
+    if (!token) res.json("error fetching token");
     const long = await ig.retrieveLongLivedToken(token.access_token);
     console.log("Long Token", long.access_token);
     try {
