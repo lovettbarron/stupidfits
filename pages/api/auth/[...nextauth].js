209507b7878dbdc66e699705dd506a456c25056c
Andrew Lovett-Barron <alb@andrewlb.com>
2020-10-06 23:11:00 +0200

    update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -21,6 +21,33 @@ const options = {
   site: process.env.HOST,
   debug: false,
   providers: [
+    // Providers.Credentials({
+    //   // The name to display on the sign in form (e.g. 'Sign in with...')
+    //   name: 'Credentials',
+    //   // The credentials is used to generate a suitable form on the sign in page.
+    //   // You can specify whatever fields you are expecting to be submitted.
+    //   // e.g. domain, username, password, 2FA token, etc.
+    //   credentials: {
+    //     username: { label: "Username", type: "text", placeholder: "jsmith" },
+    //     password: {  label: "Password", type: "password" }
+    //   },
+    //   authorize: async (credentials) => {
+
+    //     const user = await prisma.user.findOne({
+    //       where: { email: session.user.email,  },
+    //     });
+    //     if (user) {
+    //       // Any object returned will be saved in `user` property of the JWT
+    //       return Promise.resolve(user)
+    //     } else {
+    //       // If you return null or false then the credentials will be rejected
+    //       return Promise.resolve(null)
+    //       // You can also Reject this callback with an Error or with a URL:
+    //       // return Promise.reject(new Error('error message')) // Redirect to error page
+    //       // return Promise.reject('/path/to/redirect')        // Redirect to a URL
+    //     }
+    //   }
+    // }),
     Providers.Facebook({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
@@ -34,17 +61,17 @@ const options = {
         };
       },
     }),
-    // Providers.Email({
-    //   server: {
-    //     host: process.env.EMAIL_SERVER_HOST,
-    //     port: process.env.EMAIL_SERVER_PORT,
-    //     auth: {
-    //       user: process.env.EMAIL_SERVER_USER,
-    //       pass: process.env.EMAIL_SERVER_PASSWORD,
-    //     },
-    //   },
-    //   from: process.env.EMAIL_FROM,
-    // }),
+    Providers.Email({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
   ],
   secret: process.env.SECRET,
   session: {
