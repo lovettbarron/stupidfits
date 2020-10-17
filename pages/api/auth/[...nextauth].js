be4adf67ff8f5aaddf949d1c45bdd2e650461820
Andrew Lovett-Barron <alb@andrewlb.com>
2020-10-17 22:49:35 +0200

    Update to nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -21,33 +21,32 @@ const options = {
   site: process.env.HOST,
   debug: false,
   providers: [
-    // Providers.Credentials({
-    //   // The name to display on the sign in form (e.g. 'Sign in with...')
-    //   name: 'Credentials',
-    //   // The credentials is used to generate a suitable form on the sign in page.
-    //   // You can specify whatever fields you are expecting to be submitted.
-    //   // e.g. domain, username, password, 2FA token, etc.
-    //   credentials: {
-    //     username: { label: "Username", type: "text", placeholder: "jsmith" },
-    //     password: {  label: "Password", type: "password" }
-    //   },
-    //   authorize: async (credentials) => {
-
-    //     const user = await prisma.user.findOne({
-    //       where: { email: session.user.email,  },
-    //     });
-    //     if (user) {
-    //       // Any object returned will be saved in `user` property of the JWT
-    //       return Promise.resolve(user)
-    //     } else {
-    //       // If you return null or false then the credentials will be rejected
-    //       return Promise.resolve(null)
-    //       // You can also Reject this callback with an Error or with a URL:
-    //       // return Promise.reject(new Error('error message')) // Redirect to error page
-    //       // return Promise.reject('/path/to/redirect')        // Redirect to a URL
-    //     }
-    //   }
-    // }),
+    Providers.Credentials({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        const user = await prisma.user.findOne({
+          where: { email: session.user.email },
+        });
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return Promise.resolve(user);
+        } else {
+          // If you return null or false then the credentials will be rejected
+          return Promise.resolve(null);
+          // You can also Reject this callback with an Error or with a URL:
+          // return Promise.reject(new Error('error message')) // Redirect to error page
+          // return Promise.reject('/path/to/redirect')        // Redirect to a URL
+        }
+      },
+    }),
     Providers.Facebook({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
