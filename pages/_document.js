e78bb7f4601ff20d4530b691465801eceb2185a9
Andrew Lovett-Barron <alb@andrewlb.com>
2020-10-19 21:22:06 +0200

    Attempt with functions

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,35 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <title>Stupid Fits | Digital Fit Library for all your Fabrics</title>
+          {process.env.HOST !== "https://stupdifits.com" && (
+            <meta name="robots" content="noindex" />
+          )}
+          <meta property="og:title" content="Stupid Fits" key="title" />
+          <meta property="og:url" content={process.env.HOST} key="url" />
+          <meta property="og:type" content="website" key="type" />
+          <meta
+            name="keywords"
+            content="fitpics, outfits, techwear, clueless, closet, fits"
+          />
+          <meta property="fb:app_id" content={process.env.FACEBOOK_CLIENT_ID} />
 
+          <meta
+            property="og:description"
+            content="Digital Fit Library for all your Fabrics"
+          />
+          <meta property="og:site_name" content="Stupid Fits" />
+          <meta property="og:locale" content="en_US" />
+
+          <meta property="og:locale:alternate" content="en_DK" />
+          <meta property="og:locale:alternate" content="cn_CN" />
+          <meta
+            property="og:image"
+            content="https://stupidfits.com/img/appicon.png"
+            key="mainimg"
+          />
+          <meta property="og:image:width" content="1024" />
+          <meta property="og:image:height" content="1024" />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
