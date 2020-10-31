316004124d77092a4d362bad55cf59dcfb73b52d
Andrew Lovett-Barron <alb@andrewlb.com>
2020-10-31 21:10:23 +0100

    Change config of styletron app

--- pages/_app.js
+++ pages/_app.js
@@ -74,33 +74,21 @@ function App({ Component, pageProps, router }) {
         options={{ site: process.env.HOST }}
         session={pageProps.session}
       >
-        <StyletronProvider value={styletron} debugAfterHydration>
-          <BaseProvider theme={DarkTheme}>
-            <Header />
-            <Transition location={router.pathname}>
+        <Transition location={router.pathname}>
+          <StyletronProvider value={styletron} debugAfterHydration>
+            <BaseProvider theme={DarkTheme}>
+              <Header />
               <Component
                 {...pageProps}
                 url={process.env.HOST}
                 key={router.route}
               />
-            </Transition>
-          </BaseProvider>
-        </StyletronProvider>
+            </BaseProvider>
+          </StyletronProvider>
+        </Transition>
       </Provider>
     </>
   );
 }
-
-// Only uncomment this method if you have blocking data requirements for
-// every single page in your application. This disables the ability to
-// perform automatic static optimization, causing every page in your app to
-// be server-side rendered.
-//
-// MyApp.getInitialProps = async (appContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext);
-//
-//   return { ...appProps }
-// }
 
 export default App;
