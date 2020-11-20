985dea59a909c1fb12b3848dda8ac72a96ecf54e
Andrew Lovett-Barron <alb@andrewlb.com>
2020-11-20 14:02:21 +0100

    Basic tourny model

---
+++ pages/api/battle/[id].js
@@ -0,0 +1,52 @@
+import { PrismaClient } from "@prisma/client";
+import { getSession } from "next-auth/client";
+
+const prisma = new PrismaClient();
+
+// POST /api/brand
+// Required fields in body: title, authorEmail
+// Optional fields in body: content
+export default async function handle(req, res) {
+  if (req.method === "GET") {
+    const session = await getSession({ req });
+
+    const review = await prisma.collection
+      .findMany({
+        where: {
+          OR: [
+            {
+              published: true,
+            },
+            {
+              user: {
+                id: session ? session.user.id : -1,
+              },
+            },
+          ],
+        },
+        include: {
+          user: true,
+          fits: {
+            include: {
+              media: true,
+            },
+          },
+          tags: true,
+          Comment: {
+            include: {
+              user: true,
+            },
+          },
+        },
+      })
+      .finally(async () => {
+        await prisma.$disconnect();
+      });
+
+    res.json(review);
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`
+    );
+  }
+}
---
+++ pages/api/battle/create.js
@@ -0,0 +1,53 @@
+import { PrismaClient } from "@prisma/client";
+import { getSession, session } from "next-auth/client";
+import * as cloudinary from "cloudinary";
+
+const prisma = new PrismaClient();
+
+export default async function handle(req, res) {
+  const session = await getSession({ req });
+
+  if (!session || !session.user) {
+    if (context.res) {
+      context.res.writeHead(302, { Location: `/` });
+      context.res.end();
+    }
+    return {};
+  }
+
+  const user = await prisma.user.findOne({
+    where: {
+      email: session.user.email,
+    },
+  });
+
+  const review = await prisma.collection
+    .create({
+      data: {
+        user: {
+          connect: {
+            id: Number(user.id),
+          },
+        },
+        published: false,
+        title: req.body.title,
+        description: "",
+        fits: req.body.fit
+          ? {
+              connect: {
+                id: req.body.fit,
+              },
+            }
+          : undefined,
+        slug: req.body.title
+          .toLowerCase()
+          .split(" ")
+          .join("-")
+          .replace(/[^\w\s-_]/gi, ""),
+      },
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+  res.json(review);
+}
