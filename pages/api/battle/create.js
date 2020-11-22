2ee2b9c52e5236481b857439560b3e41c2f3d330
Andrew Lovett-Barron <alb@andrewlb.com>
2020-11-22 23:06:30 +0100

    Basic recursion for tournament in place

--- pages/api/battle/create.js
+++ pages/api/battle/create.js
@@ -1,6 +1,6 @@
 import { PrismaClient, BattleType } from "@prisma/client";
 import { getSession, session } from "next-auth/client";
-
+var _uuid = require("uuid");
 import generator from "tournament-generator";
 
 const prisma = new PrismaClient();
@@ -18,16 +18,42 @@ const newMatch = (round, fits, prevMatches, battle, tempid) => {
         id: battle.id,
       },
     },
-    fits: { connect: fits.map((f) => f.id) },
+    fits: fits && Array.isArray(fits) && { connect: fits.map((f) => f.id) },
     round: Number(round),
     parents: {
-      set: prevMatches.map((m) => {
-        id: m.id;
-      }),
+      set: prevMatches,
     },
     votes: null,
     _tempid: tempid,
   };
+};
+
+const setParentMatches = (step, matches) => {
+  if (step < 2) return [];
+  // Get matches for current step
+  const current = matches.filter((m) => {
+    return m.round === step;
+  });
+  // Get Unaccounted Previous matches
+  const prev = matches
+    .filter((m) => {
+      return m.round === step - 1;
+    })
+    .filter((m) => {
+      return (
+        (current.length > 1 && current.find((t) => t._tempid !== m._tempid)) ||
+        true
+      );
+    });
+
+  console.log("Parent", step, prev);
+
+  // Grab two matches from the previous unmatched
+  if (prev.length > 1) return prev.slice(0, 2).map((p) => p._tempid);
+  // Grab just the one
+  else if (prev.length === 1) return prev.slice(0, 1).map((p) => p._tempid);
+  // Return null if there isn't anything
+  else return [];
 };
 
 export default async function handle(req, res) {
@@ -63,23 +89,25 @@ export default async function handle(req, res) {
     },
   });
 
-  const battle = await prisma.battle.create({
-    startDate: "TODO",
-    endDate: "TODO",
-    rules: "",
-    type: BattleType.POPULAR,
-    user: {
-      connect: { id: user.id },
-    },
-    judges: null,
-    collection: {
-      connect: {
-        id: collection.id,
-      },
-    },
-    tags: null,
-  });
+  const battle = { id: 1 };
+  // await prisma.battle.create({
+  //   startDate: "TODO",
+  //   endDate: "TODO",
+  //   rules: "",
+  //   type: BattleType.POPULAR,
+  //   user: {
+  //     connect: { id: user.id },
+  //   },
+  //   judges: null,
+  //   collection: {
+  //     connect: {
+  //       id: collection.id,
+  //     },
+  //   },
+  //   tags: null,
+  // });
 
+  // Calculate Matches w/ library
   const { data: games } = generator(
     collection.fits.map((f) => ({
       id: f.id,
@@ -89,8 +117,9 @@ export default async function handle(req, res) {
   );
 
   console.log("Games", games);
-  // Calculate Matches
 
+  // Calculate tota number of rounds
+  // Rounding up to account for uneven
   const totalRounds = Math.ceil(Math.log2(collection.fits.length));
 
   console.log("Total Rounds", totalRounds);
@@ -100,32 +129,64 @@ export default async function handle(req, res) {
   for (let step = 1; step <= totalRounds; step++) {
     const gamesInRound = games.filter((g) => Number(g.round) === step);
 
-    if (gamesInRound) {
+    if (gamesInRound.length > 0) {
+      // If Pre-calculated
       for (let i in gamesInRound) {
         const g = gamesInRound[i];
 
-        if (g.customData) {
+        if (
+          g.customData &&
+          g.customData.constructor === Object &&
+          Object.keys(g.customData).length > 0
+        ) {
           // If has a connection from generated?
-          newMatch(
-            step,
-            [g.awayTeam.id, g.homeTeam.id],
-            [g.customData.awayTeam.id, g.customData.homeTeam.id],
-            battle,
-            g.id
+          // Not 100% sure abotu this
+          matches.push(
+            newMatch(
+              step,
+              [
+                g.awayTeam !== "TO_BE_DEFINED" && g.awayTeam.id,
+                g.homeTeam !== "TO_BE_DEFINED" && g.homeTeam.id,
+              ],
+              [g.customData.awayTeam.id, g.customData.homeTeam.id],
+              battle,
+              g.id
+            )
           );
         } else {
-          // First or second round w/ no connection
-
-          newMatch(step, [g.awayTeam.id, g.homeTeam.id], null, battle, g.id);
+          matches.push(
+            newMatch(step, [g.awayTeam.id, g.homeTeam.id], null, battle, g.id)
+          );
         }
       }
     } else {
-    }
+      // Not pre-calculated, i.e. start on 2 or 3
 
-    //round, fits, prevMatches, battle
-    newMatch(step);
+      const matchesInRound =
+        matches.filter((m) => m.round === step - 1).length / 2;
+      console.log("Matches in round", step, matchesInRound);
+      for (let mat = 0; mat < matchesInRound; mat++) {
+        const parentMatches = setParentMatches(step, matches);
+        if (parentMatches.length < 1) {
+          console.log("Something wrong with calc");
+          continue;
+        } else {
+          matches.push(
+            newMatch(
+              step,
+              [],
+              setParentMatches(step, matches),
+              battle,
+              (0, _uuid.v4)()
+            )
+          );
+        }
+      }
+    }
   }
   // Set Match objects
+
+  console.log("All Calculated Matches", JSON.stringify(matches));
 
   // Save Battle
 
